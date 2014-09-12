6e754337389361fef076dcc354cf0a3ebd3cbda6
Arnon Eilat <Arnon.Eilat@gmail.com>
2014-09-12 22:32:01 +0300

    Add dist folder.

--- dist/inflector.js
+++ dist/inflector.js
@@ -1,3 +1,26 @@
+/*! inflector.js - v1.0.0 - 2014-09-12
+* Copyright (c) 2014 ; */
+var Rule = function(expression, replacement) {
+    var expressionPattern = new RegExp(expression);
+
+
+    /**
+     * Apply the rule against the input string, returning the modified
+     * string or null if the rule didn't apply (and no modifications were
+     * made)
+     *
+     * @public
+     * @param {string} input The input string
+     * @return The modified string if this rule applied, or null if the
+     *         input was not modified by this rule
+     */
+    this.apply = function(input) {
+        if (!expressionPattern.test(input))
+            return null;
+
+        return input.replace(new RegExp(expression), replacement);
+    };
+};
 (function() {
     /**
      * @description Transforms words to singular or plural.
